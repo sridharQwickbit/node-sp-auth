b19f6d0d86195d245fe4b079f1f528eac231d7d5
pallab sarkar <pallabsarkar31@gmail.com>
2023-03-09 09:36:14 +0530

    commented

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { IAuthResponse } from './auth/IAuthResponse';
-import { IAuthOptions } from './auth/IAuthOptions';
-import { AuthResolverFactory } from './auth/AuthResolverFactory';
+// import { IAuthResponse } from './auth/IAuthResponse';
+// import { IAuthOptions } from './auth/IAuthOptions';
+// import { AuthResolverFactory } from './auth/AuthResolverFactory';
 
-export function getAuth(url: string, options?: IAuthOptions): Promise<IAuthResponse> {
-  return AuthResolverFactory.resolve(url, options).getAuth();
-}
+// export function getAuth(url: string, options?: IAuthOptions): Promise<IAuthResponse> {
+//   return AuthResolverFactory.resolve(url, options).getAuth();
+// }
 
-export * from './auth/IAuthOptions';
-export * from './auth/IAuthResponse';
-export * from './utils/TokenHelper';
-export * from './auth/base';
-export { setup, IConfiguration } from './config';
+// export * from './auth/IAuthOptions';
+// export * from './auth/IAuthResponse';
+// export * from './utils/TokenHelper';
+// export * from './auth/base';
+// export { setup, IConfiguration } from './config';
