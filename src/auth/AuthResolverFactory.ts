962cc1bf5316cbbaa323311feb61398ca9d0ae44
pallab sarkar <pallabsarkar31@gmail.com>
2023-03-09 09:35:06 +0530

    commented

--- src/auth/AuthResolverFactory.ts
+++ src/auth/AuthResolverFactory.ts
@@ -1,54 +1,54 @@
-import { IAuthResolver } from './IAuthResolver';
-import { OnpremiseFbaCredentials } from './resolvers/OnpremiseFbaCredentials';
-import { OnpremiseTmgCredentials } from './resolvers/OnpremiseTmgCredentials';
-import { OnpremiseUserCredentials } from './resolvers/OnpremiseUserCredentials';
-import { OnlineUserCredentials } from './resolvers/OnlineUserCredentials';
-import { OnlineAddinOnly } from './resolvers/OnlineAddinOnly';
-import { OnpremiseAddinOnly } from './resolvers/OnpremiseAddinOnly';
-import { AdfsCredentials } from './resolvers/AdfsCredentials';
-import { OnDemand } from './resolvers/OnDemand/OnDemand';
-import * as authOptions from './IAuthOptions';
-import { FileConfig } from './resolvers/FileConfig';
+// import { IAuthResolver } from './IAuthResolver';
+// import { OnpremiseFbaCredentials } from './resolvers/OnpremiseFbaCredentials';
+// import { OnpremiseTmgCredentials } from './resolvers/OnpremiseTmgCredentials';
+// import { OnpremiseUserCredentials } from './resolvers/OnpremiseUserCredentials';
+// import { OnlineUserCredentials } from './resolvers/OnlineUserCredentials';
+// import { OnlineAddinOnly } from './resolvers/OnlineAddinOnly';
+// import { OnpremiseAddinOnly } from './resolvers/OnpremiseAddinOnly';
+// import { AdfsCredentials } from './resolvers/AdfsCredentials';
+// import { OnDemand } from './resolvers/OnDemand/OnDemand';
+// import * as authOptions from './IAuthOptions';
+// import { FileConfig } from './resolvers/FileConfig';
 
-export class AuthResolverFactory {
-  public static resolve(siteUrl: string, options?: authOptions.IAuthOptions): IAuthResolver {
+// export class AuthResolverFactory {
+//   public static resolve(siteUrl: string, options?: authOptions.IAuthOptions): IAuthResolver {
 
-    if (!options) {
-      return new FileConfig(siteUrl);
-    }
+//     if (!options) {
+//       return new FileConfig(siteUrl);
+//     }
 
-    if (authOptions.isTmgCredentialsOnpremise(siteUrl, options)) {
-      return new OnpremiseTmgCredentials(siteUrl, options);
-    }
+//     if (authOptions.isTmgCredentialsOnpremise(siteUrl, options)) {
+//       return new OnpremiseTmgCredentials(siteUrl, options);
+//     }
 
-    if (authOptions.isFbaCredentialsOnpremise(siteUrl, options)) {
-      return new OnpremiseFbaCredentials(siteUrl, options);
-    }
+//     if (authOptions.isFbaCredentialsOnpremise(siteUrl, options)) {
+//       return new OnpremiseFbaCredentials(siteUrl, options);
+//     }
 
-    if (authOptions.isUserCredentialsOnpremise(siteUrl, options)) {
-      return new OnpremiseUserCredentials(siteUrl, options);
-    }
+//     if (authOptions.isUserCredentialsOnpremise(siteUrl, options)) {
+//       return new OnpremiseUserCredentials(siteUrl, options);
+//     }
 
-    if (authOptions.isUserCredentialsOnline(siteUrl, options)) {
-      return new OnlineUserCredentials(siteUrl, options);
-    }
+//     if (authOptions.isUserCredentialsOnline(siteUrl, options)) {
+//       return new OnlineUserCredentials(siteUrl, options);
+//     }
 
-    if (authOptions.isAddinOnlyOnline(options)) {
-      return new OnlineAddinOnly(siteUrl, options);
-    }
+//     if (authOptions.isAddinOnlyOnline(options)) {
+//       return new OnlineAddinOnly(siteUrl, options);
+//     }
 
-    if (authOptions.isAddinOnlyOnpremise(options)) {
-      return new OnpremiseAddinOnly(siteUrl, options);
-    }
+//     if (authOptions.isAddinOnlyOnpremise(options)) {
+//       return new OnpremiseAddinOnly(siteUrl, options);
+//     }
 
-    if (authOptions.isAdfsCredentials(options)) {
-      return new AdfsCredentials(siteUrl, options);
-    }
+//     if (authOptions.isAdfsCredentials(options)) {
+//       return new AdfsCredentials(siteUrl, options);
+//     }
 
-    if (authOptions.isOndemandCredentials(options)) {
-      return new OnDemand(siteUrl, options);
-    }
+//     if (authOptions.isOndemandCredentials(options)) {
+//       return new OnDemand(siteUrl, options);
+//     }
 
-    throw new Error('Error while resolving authentication class');
-  }
-}
+//     throw new Error('Error while resolving authentication class');
+//   }
+// }
